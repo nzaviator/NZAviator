5d9d00696e7ebe948a7f2e77facf3d8956bdbf58
nzaviator <120640071+nzaviator@users.noreply.github.com>
2023-01-21 18:46:09 +1300

    Update main.js

--- Decoder/js/main.js
+++ Decoder/js/main.js
@@ -5,7 +5,8 @@ const lists = {
 '///': 'Cloud is detected (unable to determine TCU/CB)',
 '////': 'Visibility not reported due faulty sensor',
 '/////////': 'Cloud not reported due faulty sensor',
-'-': 'Light',   
+'-': 'Light', 
+' ': 'Moderate (when included before a weather phenomenon)',
   'WRITE THE ABBREVIATION HERE IN ALL CAPS': 'Here is an explanation of what it means',
 };
 
